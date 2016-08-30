38bc407157dd1482c1002e2a45370e446d6c0ab8
Triani, Mario <Mario.Triani@fao.org>
2016-08-30 15:30:38 +0200

    feature-webpack-step2

---
+++ src/nls/en/labels.js
@@ -0,0 +1,21 @@
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+define(function () {
+
+    'use strict';
+
+    return {
+        renderError: "An error occurred rendering the PivotTable results.",
+        computeError: "An error occurred computing the PivotTable results.",
+        uiRenderError: "An error occurred rendering the PivotTable UI.",
+        selectAll: "Select All",
+        selectNone: "Select None",
+        tooMany: "(too many to list)",
+        filterResults: "Filter results",
+        totals: "Totals", vs: "vs", by: "by",
+        months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+    }
+});
